943f155a4ede15f8f756685f579182c8aa83cda9
Jakub Kozanowski <kuba1016@gmail.com>
2021-05-11 12:16:18 +0100

    sidebarItems connected to body

--- app/ui.R
+++ app/ui.R
@@ -1,17 +1,50 @@
 library(shiny)
 
-ui <- dashboardPage(
-  dashboardHeader(title = "Deaths in Poland 2020."),
-  dashboardSidebar(),
-  dashboardBody(
-    # Boxes need to be put in a row (or column)
-    fluidRow(
-      box(plotOutput("plot1", height = 600),
-        width = 10,
-        title = "Week by week % change in deaths registered 2019/2020",
-        solidHeader = TRUE
-      ),
-      box(selectInput("voiv", "voivodeship", choices = unique(diff_df$name)), width = 2)
+##### header#######
+
+header <- dashboardHeader(title = "Deaths in Poland 2020.")
+
+###### header end#####
+
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("Deaths by age group", tabName = "age_group"),
+    menuItem("2019 ~ 2020 deaths", tabName = "diff_tab"),
+    menuItem("Map", tabName = "map"),
+    menuItem("About", tabName = "about")
+  )
+)
+###### body #######
+
+body <- dashboardBody(
+  ## tab Items##
+  tabItems(
+    tabItem(
+      tabName = "age_group",
+      h2("Dashboard age_group content")
+    ),
+    tabItem(
+      tabName = "diff_tab",
+      fluidRow(
+        box(plotOutput("plot1", height = 600),
+          width = 10,
+          title = "Week by week % change in deaths registered 2019/2020",
+          solidHeader = TRUE
+        ),
+        box(selectInput("voiv", "voivodeship", choices = unique(diff_df$name)), width = 2)
+      )
+    ),
+    tabItem(
+      tabName = "map",
+      h2("Dashboard map content")
+    ),
+    tabItem(
+      tabName = "about",
+      h2("Dashboard about content")
     )
   )
 )
+
+###### body end#####
+
+dashboardPage(header, sidebar, body)
